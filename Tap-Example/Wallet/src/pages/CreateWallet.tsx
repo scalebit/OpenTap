bf145799fd34a3005f329553f8f2ab088139fb81
yoyoping <zhangping_ymd@outlook.com>
2024-05-20 16:21:23 +0800

    Update UI

--- Tap-Example/Wallet/src/pages/CreateWallet.tsx
+++ Tap-Example/Wallet/src/pages/CreateWallet.tsx
@@ -7,15 +7,30 @@ export default () => {
         setStep(2)
     }
 
-    const [keyNumber, setKeyNumber] = useState(1)
+    const [keyNumber, setKeyNumber] = useState<number>(1)
     const UpdateKeyNumber = (e: any) => {
-        setKeyNumber(e.target.value)
-        setThreshold(1)
+        if (e.target.value >= 1) {
+            setKeyNumber(e.target.value)
+            setThreshold(1)
+
+            const newValues = Array(+e.target.value).fill('').map((_, i) => publicKeyArr[i] || '');
+            setPublicKeyArr(newValues);
+        }
     }
 
-    const [threshold, setThreshold] = useState(1)
+    const [threshold, setThreshold] = useState<number>(1)
     const UpdateThreshold = (e: any) => {
-        setThreshold(e.target.value)
+        if (e.target.value >= 1) {
+            setThreshold(e.target.value)
+        }
+    }
+
+    const [publicKeyArr, setPublicKeyArr] = useState<string[]>(Array(keyNumber).fill(''));
+
+    const UpdatePublicKeyArr = (index: number, value: string) => {
+        const newValues = [...publicKeyArr]
+        newValues[index] = value
+        setPublicKeyArr(newValues)
     }
 
     return (
@@ -25,20 +40,23 @@ export default () => {
                     step === 1 ? <section>
                         <div className="flex justify-between items-center mb-3">
                             <label className="w-[100px] block">Key Number</label>
-                            <input type="range" min={1} max={10} value={keyNumber} onChange={UpdateKeyNumber} className="range range-xs mx-10 flex-1 range-info" />
-                            <input type="number" placeholder="" value={keyNumber} className="input input-bordered w-[100px] input-md" />
+                            <input type="range" min={1} max={9} value={keyNumber} onChange={UpdateKeyNumber} className="range range-xs mx-10 flex-1 range-info" />
+                            <input type="number" min={1} max={9} placeholder="" value={keyNumber} onChange={UpdateKeyNumber} className="input input-bordered w-[100px] input-md" />
                         </div>
                         <div className="flex justify-between items-center mb-3">
                             <label className="w-[100px] block">Threshold</label>
                             <input type="range" min={1} max={keyNumber} value={threshold} onChange={UpdateThreshold} className="range range-xs mx-10 flex-1 range-info" />
-                            <input type="number" placeholder="" value={threshold} className="input input-bordered w-[100px] input-md" />
+                            <input type="number" min={1} max={keyNumber} placeholder="" value={threshold} onChange={UpdateThreshold} className="input input-bordered w-[100px] input-md" />
                         </div>
                         <div className="">
                             <label className="w-[100px] block mb-3">Public Key</label>
-                            <input type="text" placeholder="" className="input input-bordered w-full input-md mb-3" />
-                            <input type="text" placeholder="" className="input input-bordered w-full input-md mb-3" />
-                            <input type="text" placeholder="" className="input input-bordered w-full input-md mb-3" />
-                            <input type="text" placeholder="" className="input input-bordered w-full input-md mb-3" />
+                            <div className="h-[240px] overflow-y-auto pr-1">
+                                {
+                                    Array.from({ length: keyNumber }, (_, index) => (
+                                        <input type="text" key={index} value={publicKeyArr[index] || ''} placeholder="" onChange={(e) => UpdatePublicKeyArr(index, e.target.value)} className="input input-bordered w-full input-md mt-3" />
+                                    ))
+                                }
+                            </div>
                         </div>
                         <button className="btn btn-primary btn-wide mt-10 mx-auto block" onClick={confirm}>Confirm</button>
                     </section> : <section className="recipet">
