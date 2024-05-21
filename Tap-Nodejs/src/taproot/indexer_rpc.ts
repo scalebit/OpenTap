93c7f5fc3b2db04b9e479225c270380ed54df6a7
zorrot <ui_zorrot@163.com>
2024-05-21 20:14:46 +0800

    add indexer_rpc

---
+++ Tap-Nodejs/src/taproot/indexer_rpc.ts
@@ -0,0 +1,6 @@
+import axios, { AxiosResponse } from "axios";
+import { IUTXO } from "./utils.js"
+
+// baseURL: `https://blockstream.info/testnet/api`
+
+const URL = `http://user:pass@127.0.0.1:18443/`
---
+++ Tap-Nodejs/src/template/indexer.ts
@@ -0,0 +1,5 @@
+async function start() {
+    console.log("suc")
+}
+
+start().then(() => process.exit());
