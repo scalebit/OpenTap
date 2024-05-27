41bb0b4959b45dfc770e3b144bc8d1190a55d881
yoyoping <zhangping_ymd@outlook.com>
2024-05-27 16:58:00 +0800

    完善前端交互

--- Tap-Example/Wallet/src/pages/CreateWallet.tsx
+++ Tap-Example/Wallet/src/pages/CreateWallet.tsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react'
-import { useStore } from '../store/index'
 import { asm_builder, p2tr_wallet } from 'open-tap-nodejs/src/taproot/taproot_script_builder'
+import { useStore } from '../store/index'
+import { PublicKey } from '../config/interface'
+// import { downloadJSON } from '../config/index'
 
 const CreateWallet = () => {
     const { network, publicKey } = useStore()
+
+    const [isImport, setIsImport] = useState(false)
 
     const [step, setStep] = useState(1)
 
     const [walletName, setWalletName] = useState('')
 
-    const [publicKeyArr, setPublicKeyArr] = useState([{
+    const [publicKeyArr, setPublicKeyArr] = useState<PublicKey[]>([{
         tag: '',
         publicKey: ''
     },
@@ -21,8 +25,6 @@ const CreateWallet = () => {
     const [threshold, setThreshold] = useState(1)
 
     const [descriptor, setDescriptor] = useState('vuwuevbbseviubweuirbviuaehriyvbkhebrviyebriyvbiwEBSVIUIAEVUYEGSUKRGVUEYRGF7458GERUGIVYGEAR8OTG4875EG487E5YGH45HG45HG9H94G')
-
-    console.log(setDescriptor)
 
     useEffect(() => {
         if (publicKey) {
@@ -52,33 +54,60 @@ const CreateWallet = () => {
         setPublicKeyArr(arr_)
     }
 
-    const nextStep = () => {
-        if (step === 2) {
-            if (!walletName) {
-                return
-            }
-        } else if (step === 3) {
-            const flag = publicKeyArr.every(item => item.tag && item.publicKey)
-            if (!flag || threshold < 1 || threshold > publicKeyArr.length) {
-                return
-            }
-            //TODO:检查所有的publickey是否为32位，如果不是则return
-            console.log("step3")
-            //创建对应的地址和解锁脚本
-            const pks: string[] = publicKeyArr.map(item => item.publicKey)
-            const script = asm_builder(pks, threshold)
-            const { p2tr, redeem } = p2tr_wallet(script, pks)
-
-            //TODO:生成JSON并保存在LocalStroge中，并标记为（wallet + 编号 + 之前命名的名称）
-            localStorage.setItem(walletName + '-address', JSON.stringify(p2tr));
-            localStorage.setItem(walletName + '-reedem', JSON.stringify(redeem));
+    const nextStep = (isImport_?: boolean) => {
+        if (step === 1) {
+            setIsImport(isImport_ || false)
         }
 
-        setStep(step + 1)
+        if (isImport) {
+            if (step === 2) {
+                if (!descriptor) {
+                    return
+                }
+            } else if (step === 3) {
+                if (!walletName) {
+                    return
+                }
+            }
+        } else {
+            if (step === 2) {
+                if (!walletName) {
+                    return
+                }
+            } else if (step === 3) {
+                const flag = publicKeyArr.every(item => item.tag && item.publicKey)
+                if (!flag || threshold < 1 || threshold > publicKeyArr.length) {
+                    return
+                }
+
+                //TODO:检查所有的publickey是否为32位，如果不是则return
+                console.log("step3")
+                //创建对应的地址和解锁脚本
+                const pks: string[] = publicKeyArr.map(item => item.publicKey)
+                const script = asm_builder(pks, threshold)
+
+                const { p2tr, redeem } = p2tr_wallet(script, pks)
+                console.log(p2tr, redeem)
+
+                // //TODO:生成JSON并保存在LocalStroge中，并标记为（wallet + 编号 + 之前命名的名称）
+                localStorage.setItem(walletName + '-address', JSON.stringify(p2tr));
+                localStorage.setItem(walletName + '-reedem', JSON.stringify(redeem));
+            }
+        }
+        if (step <= 3) {
+            setStep(step + 1)
+        }
     }
 
     const handleCreate = () => {
-
+        if (isImport) {
+            if (step === 4) {
+                const flag = publicKeyArr.every(item => item.tag)
+                if (!flag) {
+                    return
+                }
+            }
+        }
     }
 
     const copyToClipboard = () => {
@@ -88,6 +117,108 @@ const CreateWallet = () => {
             console.error("copy error: ", err);
         });
     }
+
+    const Step2DOM = isImport ? <div>
+        <p className='font-bold mb-5'>Step 2: Import</p>
+        <p>Please input the descriptor in the box.</p>
+        <textarea className="textarea input-bordered my-5 w-[600px] h-[150px]" value={descriptor} onChange={(e) => setDescriptor(e.target.value)}></textarea>
+        <p>Only Support OpenTap Wallet.</p>
+        <div className='flex mt-10'>
+            <button className='btn w-[150px] mr-5' onClick={() => setStep(step - 1)}>Back</button>
+            <button className='btn w-[150px] btn-primary' onClick={() => nextStep()}>Confirm</button>
+        </div>
+    </div> : <div>
+        <p className='font-bold mb-5'>Step 2: Information</p>
+        <p>Choose the Name and Network of your new wallet.</p>
+        <label className="input input-bordered flex items-center gap-2 w-[400px] my-6">
+            <input type="text" className="grow" placeholder="" value={walletName} onChange={(e) => setWalletName(e.target.value)} />
+            <span className="badge badge-info">{network}</span>
+        </label>
+        <p>Please <span className='font-bold'>Confirm</span> your setting.</p>
+        <div className='flex mt-10'>
+            <button className='btn w-[150px] mr-5' onClick={() => setStep(step - 1)}>Back</button>
+            <button className='btn w-[150px] btn-primary' onClick={() => nextStep()}>Confirm</button>
+        </div>
+    </div>
+
+    const Step3DOM = isImport ? <div>
+        <p className='font-bold mb-5'>Step 3: Information</p>
+        <p>Choose the Name of your new wallet.</p>
+        <label className="input input-bordered flex items-center gap-2 w-[400px] my-6">
+            <input type="text" className="grow" placeholder="" value={walletName} onChange={(e) => setWalletName(e.target.value)} />
+            <span className="badge badge-info">{network}</span>
+        </label>
+        <p>Please <span className='font-bold'>Confirm</span> your setting.</p>
+        <div className='flex mt-10'>
+            <button className='btn w-[150px] mr-5' onClick={() => setStep(step - 1)}>Back</button>
+            <button className='btn w-[150px] btn-primary' onClick={() => nextStep()}>Confirm</button>
+        </div>
+    </div> : <div>
+        <p className='font-bold mb-5'>Step 3: Multi-Sig</p>
+        <p>Create Threshold Multi-Sig Wallet or Normal Wallet.</p>
+
+        <div className='my-5 w-[600px]'>
+            {
+                publicKeyArr.map((item: PublicKey, index: number) => <div key={index} className='flex mb-3'>
+                    <input type="text" placeholder="Tag" value={item.tag} onChange={(e) => changePK('tag', e.target.value, index)} className="input input-bordered w-[100px] mr-2" />
+                    <input type="text" placeholder="Publick Key" disabled={index === 0} value={item.publicKey} onChange={(e) => changePK('publicKey', e.target.value, index)} className="input input-bordered flex-1" />
+                    {
+                        index > 0 ? <button className="btn btn-circle ml-2" onClick={() => delPublicKey(index)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        </button> : <div className='w-[48px] h-[48px] ml-2'></div>
+                    }
+                </div>)
+            }
+            <button className="btn btn-outline w-full text-[24px]" onClick={addPublicKey}>+</button>
+            <div className='my-10'>
+                <h3 className='font-bold mb-5'>Threshold</h3>
+                <div>
+                    <input type='number' value={threshold} min={1} max={publicKeyArr.length} onChange={(e) => setThreshold(+e.target.value)} className='input input-bordered w-[80px] mr-3' />
+                    <span>Out of {publicKeyArr.length}</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='flex mt-10'>
+            <button className='btn w-[150px] mr-5' onClick={() => setStep(step - 1)}>Back</button>
+            <button className='btn w-[150px] btn-primary' onClick={() => nextStep()}>Confirm</button>
+        </div>
+    </div>
+
+    const Step4DOM = isImport ? <div>
+        <p className='font-bold mb-5'>Step 4: Tag</p>
+        <p>Create Threshold Multi-Sig Wallet or Normal Wallet.</p>
+
+        <div className='my-5 w-[600px]'>
+            {
+                publicKeyArr.map((item: PublicKey, index: number) => <div key={index} className='flex mb-3'>
+                    <input type="text" placeholder="Tag" value={item.tag} onChange={(e) => changePK('tag', e.target.value, index)} className="input input-bordered w-[100px] mr-2" />
+                    <input type="text" placeholder="Publick Key" disabled value={item.publicKey} onChange={(e) => changePK('publicKey', e.target.value, index)} className="input input-bordered flex-1" />
+                </div>)
+            }
+            <div className='my-10'>
+                <h3 className='font-bold mb-5'>Threshold</h3>
+                <div>
+                    <input type='number' value={threshold} disabled min={1} max={publicKeyArr.length} onChange={(e) => setThreshold(+e.target.value)} className='input input-bordered w-[80px] mr-3' />
+                    <span>Out of {publicKeyArr.length}</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='flex mt-10'>
+            <button className='btn w-[150px] mr-5' onClick={() => setStep(step - 1)}>Back</button>
+            <button className='btn w-[150px] btn-primary' onClick={() => nextStep()}>create</button>
+        </div>
+    </div> : <div>
+        <p className='font-bold mb-5'>Step 4: Success</p>
+        <p>To recover the wallet you MUST using the descriptor.</p>
+        <textarea className="textarea my-5 w-[600px] h-[150px]" value={descriptor} disabled></textarea>
+        <p>Copy and save the descriptor, then finish the creation.</p>
+        <div className='flex mt-10'>
+            <button className='btn w-[150px] mr-5' onClick={handleCreate}>Create</button>
+            <button className='btn w-[150px] btn-primary' onClick={copyToClipboard}>Copy</button>
+        </div>
+    </div>
 
     return (
         <div>
@@ -106,62 +237,10 @@ const CreateWallet = () => {
                     <p className='my-2'>please assgin with a third-party wallet.</p>
                     <p>Or using descriptor to Import a wallet.</p>
                     <div className='flex mt-10'>
-                        <button className='btn w-[150px] btn-primary mr-5' onClick={nextStep}>Next</button>
-                        <button className='btn w-[150px] btn-neutral'>Import</button>
+                        <button className='btn w-[150px] btn-primary mr-5' onClick={() => nextStep(false)}>Next</button>
+                        <button className='btn w-[150px] btn-neutral' onClick={() => nextStep(true)}>Import</button>
                     </div>
-                </div> : step === 2 ? <div>
-                    <p className='font-bold mb-5'>Step 2: Information</p>
-                    <p>Choose the Name and Network of your new wallet.</p>
-                    <label className="input input-bordered flex items-center gap-2 w-[400px] my-6">
-                        <input type="text" className="grow" placeholder="" value={walletName} onChange={(e) => setWalletName(e.target.value)} />
-                        <span className="badge badge-info">{network}</span>
-                    </label>
-                    <p>Please <span className='font-bold'>Confirm</span> your setting.</p>
-                    <div className='flex mt-10'>
-                        <button className='btn w-[150px] mr-5' onClick={() => setStep(step - 1)}>Back</button>
-                        <button className='btn w-[150px] btn-primary' onClick={nextStep}>Confirm</button>
-                    </div>
-                </div> : step === 3 ? <div>
-                    <p className='font-bold mb-5'>Step 3: Multi-Sig</p>
-                    <p>Create Threshold Multi-Sig Wallet or Normal Wallet.</p>
-
-                    <div className='my-5 w-[600px]'>
-                        {
-                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                            publicKeyArr.map((item: any, index: number) => <div key={index} className='flex mb-3'>
-                                <input type="text" placeholder="Tag" value={item.tag} onChange={(e) => changePK('tag', e.target.value, index)} className="input input-bordered w-[100px] mr-2" />
-                                <input type="text" placeholder="Publick Key" disabled={index === 0} value={item.publicKey} onChange={(e) => changePK('publicKey', e.target.value, index)} className="input input-bordered flex-1" />
-                                {
-                                    index > 0 ? <button className="btn btn-circle ml-2" onClick={() => delPublicKey(index)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                                    </button> : <div className='w-[48px] h-[48px] ml-2'></div>
-                                }
-                            </div>)
-                        }
-                        <button className="btn btn-outline w-full text-[24px]" onClick={addPublicKey}>+</button>
-                        <div className='my-10'>
-                            <h3 className='font-bold mb-5'>Threshold</h3>
-                            <div>
-                                <input type='number' value={threshold} min={1} max={publicKeyArr.length} onChange={(e) => setThreshold(+e.target.value)} className='input input-bordered w-[80px] mr-3' />
-                                <span>Out of {publicKeyArr.length}</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='flex mt-10'>
-                        <button className='btn w-[150px] mr-5' onClick={() => setStep(step - 1)}>Back</button>
-                        <button className='btn w-[150px] btn-primary' onClick={nextStep}>Confirm</button>
-                    </div>
-                </div> : <div>
-                    <p className='font-bold mb-5'>Step 4: Success</p>
-                    <p>To recover the wallet you MUST using the descriptor.</p>
-                    <textarea className="textarea my-5 w-[600px] h-[150px]" value={descriptor} disabled></textarea>
-                    <p>Copy and save the descriptor, then finish the creation.</p>
-                    <div className='flex mt-10'>
-                        <button className='btn w-[150px] mr-5' onClick={handleCreate}>Create</button>
-                        <button className='btn w-[150px] btn-primary' onClick={copyToClipboard}>Copy</button>
-                    </div>
-                </div>
+                </div> : step === 2 ? Step2DOM : step === 3 ? Step3DOM : Step4DOM
             }
         </div>
     )
