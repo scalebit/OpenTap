3ca634b8b9a05d682a990a16a676cb143b629fb2
yoyoping <zhangping_ymd@outlook.com>
2024-05-29 17:06:58 +0800

    修复浏览器Buffer未定义的bug

--- Tap-Example/Wallet/vite.config.ts
+++ Tap-Example/Wallet/vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import wasm from 'vite-plugin-wasm';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import path from "path"
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react(), wasm(), nodePolyfills()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
