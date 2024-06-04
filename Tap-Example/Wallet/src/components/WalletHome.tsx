712bf9e554a0d9a8ead34a2e8a403313e08236e8
zorrot <ui_zorrot@163.com>
2024-06-04 17:46:12 +0800

    fix add

--- Tap-Example/Wallet/src/components/WalletHome.tsx
+++ Tap-Example/Wallet/src/components/WalletHome.tsx
@@ -2,12 +2,13 @@
 const WalletHome = (props: any) => {
     const { currentWallet } = props
     const descriptor = "123"
+    const amount = "20.32340000"
     return (
         <div>
             <h3 className="text-[16px] font-bold mb-3">Your Address:</h3>
             <p className='w-[360px] break-words'>{currentWallet.p2tr.address}</p>
             <h3 className="text-[16px] font-bold mb-3 mt-10">Bitcoin Amount:</h3>
-            <p className='text-[blue] font-bold mb-3'>0.0025</p>
+            <p className='text-[blue] font-bold mb-3'>{amount}</p>
             <p>{currentWallet.publicKeyArr.length}-{currentWallet.threshold} Threshold Account</p>
             <h3 className="text-[16px] font-bold mb-3 mt-10"> Descriptor</h3>
             <p>Use descriptor to recover the account</p>
